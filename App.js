a01732aac2c2ac117c45ecaf6aa8e6892296f69f
Martin Babak <msb1093@unh.edu>
2023-09-12 23:31:40 -0400

    Add another styles

--- App.js
+++ App.js
@@ -2,18 +2,36 @@ import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  const [word, setWord] = useState("niggas");
+  const [enetredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enetredGoalText,
+    ]);
+    setEnteredGoalText("");
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="Your next course goal"
+          onChangeText={goalInputHandler}
         />
-        <Button title="Add goal" />
+        <Button title="Add goal" onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((text, id) => {
+          return (
+            <Text style={styles.goalItem} key={id}>
+              {text}
+            </Text>
+          );
+        })}
       </View>
     </View>
   );
@@ -21,17 +39,34 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#000000",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
-    padding: 5,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
   },
 });
